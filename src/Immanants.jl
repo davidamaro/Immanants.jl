d521fb76b7b6a070daa7db857b73804110ae341f
David Amaro Alcalá <dav1494@ciencias.unam.mx>
2020-09-08 16:15:53 -0500

    se añade el método immanant. Faltan tests y arreglar los tipos.

--- src/Immanants.jl
+++ src/Immanants.jl
@@ -1,5 +1,26 @@
 module Immanants
 
-greet() = print("Hello World!")
+import AbstractAlgebra: character, Partition, @perm_str, Perm
+import Combinatorics: permutations
+
+export immanant, Partition
+
+function immanant(λ, mat::Array{Complex{T},2}) where T <: Real
+    len = sum(λ)
+    chi = character(λ)
+    total = zero(T)
+    prod = one(T)
+    for p in permutations(1:len |> collect)
+        ph = p |> sortperm
+        for i in 1:len
+            prod *= mat[i, ph[i]]
+        end
+        prod *= chi(Perm(ph))
+        total += prod
+
+        prod = one(T)
+    end
+    total
+end
 
 end # module
