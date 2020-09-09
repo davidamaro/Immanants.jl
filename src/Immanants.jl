8032772d5de6f2da76a2439b20a9d5a33a8dc879
David Amaro Alcalá <dav1494@ciencias.unam.mx>
2020-09-09 17:15:05 -0500

    version que funciona.

--- src/Immanants.jl
+++ src/Immanants.jl
@@ -5,6 +5,24 @@ import Combinatorics: permutations
 
 export immanant, Partition
 
+function immanant(λ, mat::Array{T,2}) where T <: Real
+    len = sum(λ)
+    chi = character(λ)
+    total = zero(T)
+    prod = one(T)
+    for p in permutations(1:len |> collect)
+        ph = p |> sortperm
+        for i in 1:len
+            prod *= mat[i, ph[i]]
+        end
+        prod *= chi(Perm(ph))
+        total += prod
+
+        prod = one(T)
+    end
+    total
+end
+
 function immanant(λ, mat::Array{Complex{T},2}) where T <: Real
     len = sum(λ)
     chi = character(λ)
